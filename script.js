7dda8d8402052c675946311e325f11f97359b568
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-24 16:42:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 //your JS code here. If required.
-let bodyele=document.querrySelector("body");
-let para=document.createElement("p");
-para.id="counter";
-bodyele.appendChild(para);
-// let i=1;
-para.innerText="abc";
-let btn=document.createElement("Button");
+
 
