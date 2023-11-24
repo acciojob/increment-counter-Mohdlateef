b00ce213d86af0aea7bbd01735c430fa44f5bda5
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-24 17:01:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 //your JS code here. If required.
-
+let paragraph=document.querrySelector("#para");
+let i=0;
+paragraph.innerText=i.toString();
+let btn=document.querrySelector("#incrementBtn");
+btn.addEventListenser("click",() =>{
+	alert(i);
+})
 
